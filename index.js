33bb1b4099740c65aa0c3dead3950aa45576bbd6
archanavnair <archanavnair90@gmail.com>
2024-02-02 21:31:22 -0500

    my Todos Application

---
+++ index.js
@@ -0,0 +1,62 @@
+const textarea = document.querySelector('textarea');
+const addBtn = document.getElementById('addBtn');
+const todoContainer = document.querySelector('.todoContainer');
+
+let todoList = [];
+
+function initialLoad(){
+    if(!localStorage.getItem('todos')){return}
+    todoList = JSON.parse(localStorage.getItem('todos')).todoList
+    updateUI();
+}
+
+initialLoad()
+
+function addTodo() {
+    const todo = textarea.value;
+    if(!todo){
+        return
+    }
+    console.log('Add todo: ',todo);
+    todoList.push(todo)
+    textarea.value = '';
+    updateUI();
+}
+function updateUI(){
+    let newInnerHtml = ''; 
+
+    todoList.forEach((todoElement,todoIndex)=>{
+        
+        newInnerHtml += `<div class="todo">
+        <p>${todoElement}</p>
+        <div class="btnContainer">
+            <button class="iconBtn" onclick="editTodo(${todoIndex})"><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="iconBtn" onclick="deleteTodo(${todoIndex})"><i class="fa-solid fa-trash"></i></button>
+        </div>
+    </div>`
+    })
+
+    todoContainer.innerHTML = newInnerHtml
+    //to save to local storage
+    localStorage.setItem('todos',JSON.stringify({todoList}))
+}
+
+function editTodo(index){
+    textarea.value = todoList[index]
+    todoList = todoList.filter((element,elementIndex) => {
+        if(index === elementIndex){return false}
+        return true
+    })
+
+    updateUI();
+}
+
+function deleteTodo(index){
+    todoList = todoList.filter((element,elementIndex) => {
+        if(index === elementIndex){return false}
+        return true
+    })
+    updateUI();
+}
+
+addBtn.addEventListener('click',addTodo )
